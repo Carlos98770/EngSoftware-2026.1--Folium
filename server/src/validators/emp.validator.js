7d9945a5b0656ec0a58ad485ee6689c48e5c25cf
João Victor do Nascimento Ribeiro de Andrade <153670689+JoaoVictorNascimento1@users.noreply.github.com>
2026-06-01 16:09:03 -0300

    correcao-linha4

--- server/src/validators/emp.validator.js
+++ server/src/validators/emp.validator.js
@@ -1,8 +1,7 @@
 import { body } from 'express-validator';
 
 export const emprestimoCreateValidator = [
-    body('livro_id')
-        .isMongoId().withMessage('ID do livro inválido'),
+    body('livro_id').isInt().withMessage('ID do livro deve ser um número inteiro'),
 
     body('data_fim')
         .isISO8601().withMessage('Data final inválida (use o formato ANO-MES-DIA)')
