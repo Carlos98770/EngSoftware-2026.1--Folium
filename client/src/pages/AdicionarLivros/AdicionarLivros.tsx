96ea73f38651744b683eeea5d7ffae1625b34bce
dka0086 <getuliofariasegundo@gmail.com>
2026-06-02 19:02:16 -0300

    Alteracao rapida do css dos formularios

--- client/src/pages/AdicionarLivros/AdicionarLivros.tsx
+++ client/src/pages/AdicionarLivros/AdicionarLivros.tsx
@@ -3,7 +3,7 @@ import { authService } from "../../auth/AuthService"
 import { LivroService } from "../../services/LivroService"
 import * as z from "zod"
 import { toast } from "react-toastify"
-//import "./LoginForm.css"
+import "./AdicionarLivro.css"
 
 type FormErros = {
     nome: string,
@@ -113,43 +113,50 @@ export default function AdicionarPage() {
     const messagens = mensagensErros(erros)
 
     return (
-        <div className="MainContainer">
-            <div className="LoginForm">
-                <h1 className="LoginText">Adicionar Livro</h1>
-                <form action="" id="formLivro">
-                    <h4>Preencha os dados do livro:</h4>
-                    <div className="EmailForm">
-                        <label htmlFor="nome">Nome:</label>
-                        <input id="nome" type="text" value={formdata.nome} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.nomeRef} />
-                    </div>
-                    <div className="EmailForm">
-                        <label htmlFor="editora">Editora:</label>
-                        <input id="editora" type="text" value={formdata.editora} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.editoraRef} />
-                    </div>
-                    <div className="EmailForm">
-                        <label htmlFor="comentario">Comentário:</label>
-                        <input id="comentario" type="text" value={formdata.comentario} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.comentarioRef} />
-                    </div>
-                    <div className="SenhaForm">
-                        <label htmlFor="quantidade_total">Quantidade:</label>
-                        <input id="quantidade_total" type="number" value={formdata.quantidade_total} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.quantidadeRef} />
-                    </div>
-                </form>
+        <div className="AddBookContainer">
+    <div className="AddBookCard">
+        <h1 className="AddBookTitle">Adicionar Livro</h1>
+        
+        <form action="" id="formLivro">
+            <h4>Preencha os dados do livro:</h4>
+            
+            <div className="FormGroup">
+                <label htmlFor="nome">Nome:</label>
+                <input id="nome" type="text" value={formdata.nome} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.nomeRef} />
             </div>
-            <div className="ButtonsLoginForm">
-                <span><button className="ConfirmLogin" onClick={handleSubmitClick}>Adicionar</button></span>
+            
+            <div className="FormGroup">
+                <label htmlFor="editora">Editora:</label>
+                <input id="editora" type="text" value={formdata.editora} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.editoraRef} />
             </div>
-            <div className="MensagensErros">
-                {messagens.length > 0 && (
-                    <div>
-                        <ul>
-                            {messagens.map((error, index) => (
-                                <li key={index}>{error}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+            
+            <div className="FormGroup">
+                <label htmlFor="comentario">Comentário:</label>
+                <input id="comentario" type="text" value={formdata.comentario} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.comentarioRef} />
             </div>
+            
+            <div className="FormGroup">
+                <label htmlFor="quantidade_total">Quantidade:</label>
+                <input id="quantidade_total" type="number" value={formdata.quantidade_total} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.quantidadeRef} />
+            </div>
+        </form>
+
+        <div className="AddBookActions">
+            <button className="AddBookBtn" onClick={handleSubmitClick}>Adicionar</button>
         </div>
+
+        <div className="AddBookErrors">
+            {messagens.length > 0 && (
+                <div>
+                    <ul>
+                        {messagens.map((error, index) => (
+                            <li key={index}>{error}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    </div>
+</div>
     )
 }
