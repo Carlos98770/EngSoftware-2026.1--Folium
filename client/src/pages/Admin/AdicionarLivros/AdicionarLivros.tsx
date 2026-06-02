38218ed67b8616d45af1e6cc109e09e6c576f792
dka0086 <getuliofariasegundo@gmail.com>
2026-06-02 17:09:25 -0300

    Formulario para adicionar livros

--- client/src/pages/Admin/AdicionarLivros/AdicionarLivros.tsx
+++ client/src/pages/Admin/AdicionarLivros/AdicionarLivros.tsx
@@ -1,27 +1,29 @@
 import { useState, useRef } from "react"
-import { useNavigate } from "react-router-dom"
 import { authService } from "../../../auth/AuthService"
-import { accountService } from "../../../services/AccountService"
-import type { LoginUser } from "../../../models/LoginUser"
+import { livroService } from "../../../services/LivroService"
 import * as z from "zod"
 import { toast } from "react-toastify"
 import "./LoginForm.css"
 
 type FormErros = {
-    email: string,
-    senha: string
+    nome: string,
+    editora: string,
+    comentario: string,
+    quantidade_total: string,
+    quantidade_disponivel: string,
 }
 
-export default function RegisterForm(){
+export default function LivroForm() {
     const [formdata, setFormData] = useState({
-        email: "",
-        senha: ""
+        nome: "",
+        editora: "",
+        comentario: "",
+        quantidade_total: 0,
+        quantidade_disponivel: 0,
     })
 
-    const navigate = useNavigate()
-
     const onChangeFormData = (evt) => {
-        const {id, value} = evt.target
+        const { id, value } = evt.target
         setFormData(prev => ({
             ...prev,
             [id]: value
@@ -29,73 +31,71 @@ export default function RegisterForm(){
     }
 
     const inputRefs = {
-        emailRef: useRef<HTMLInputElement>(null),
         nomeRef: useRef<HTMLInputElement>(null),
-        senhaRef: useRef<HTMLInputElement>(null),
+        editoraRef: useRef<HTMLInputElement>(null),
+        comentarioRef: useRef<HTMLInputElement>(null),
+        quantidadeRef: useRef<HTMLInputElement>(null),
+        quantidadedispRef: useRef<HTMLInputElement>(null),
     }
-    
+
     const [erros, setErrors] = useState<FormErros>({
-        email: "",
-        senha: ""
+        nome: "",
+        editora: "",
+        comentario: "",
+        quantidade_total: "",
+        quantidade_disponivel: "",
     })
 
-    const loginInput = z.object({
-            email: z.string().min(1,"O campo de email é obrigatório").max(100, "O email excede o limite de 100 caracteres")
-            .refine((input) => input==="admin" || z.email().safeParse(input).success,
-        "Email inválido"),
-            senha: z.string().min(6, "A senha deve ter no minimo 6 caracteres")
-            .max(50,"A senha excede o limite de 50 caracteres")
+    const livroInput = z.object({
+        nome: z.string().min(1, "O nome é obrigatório").max(255, "Nome excede 255 caracteres"),
+        editora: z.string().min(1, "A editora é obrigatória").max(255, "Editora excede 255 caracteres"),
+        comentario: z.string().max(200, "Comentário excede 200 caracteres").optional(),
+        quantidade_total: z.coerce.number().min(1, "A quantidade deve ser maior que 0"),
+        quantidade_disponivel: z.coerce.number().min(1, "A quantidade deve ser maior que 0"),
     })
 
-    const validateForm = async(formdata) => {
-        const validacao = loginInput.safeParse(formdata)
+    const validateForm = async (formdata) => {
+        const validacao = livroInput.safeParse(formdata)
 
-        if(!validacao.success){
-            const loginErros = z.treeifyError(validacao.error) //.flatten().fieldErrors
+        if (!validacao.success) {
+            const livroErros = z.treeifyError(validacao.error)
             const mensagens: FormErros = {
-            email: loginErros.properties.email?.errors[0] ?? "",
-            senha: loginErros.properties.senha?.errors[0] ?? ""
-        }
-        setErrors(mensagens)
-        return { valido: false, messagens: mensagens }
+                nome: livroErros.properties.nome?.errors[0] ?? "",
+                editora: livroErros.properties.editora?.errors[0] ?? "",
+                comentario: livroErros.properties.comentario?.errors[0] ?? "",
+                quantidade_total: livroErros.properties.quantidade_total?.errors[0] ?? "",
+                quantidade_disponivel: livroErros.properties.quantidade_total?.errors[0] ?? "",
+            }
+            setErrors(mensagens)
+            return { valido: false, messagens: mensagens }
         }
 
-    setErrors({ email: "", senha: "" })
-    return { valido: true, messagens: { email: "", senha: "" } }
+        setErrors({ nome: "", editora: "", comentario: "", quantidade_total: "" , quantidade_disponivel: ""})
+        return { valido: true, messagens: { nome: "", editora: "", comentario: "", quantidade_total: "", quantidade_disponivel: "" } }
     }
 
-    const formBlurs = async(evt) => {
+    const formBlurs = async (evt) => {
         const { id, value } = evt.target
         setFormData(prev => {
-        const updated = { ...prev, [id]: value }
+            const updated = { ...prev, [id]: value }
             validateForm(updated)
             return updated
         })
     }
 
-    const handleLoginClick = async() => {
+    const handleSubmitClick = async () => {
         const { valido, messagens } = await validateForm(formdata)
-        if(valido) {
-            const user: LoginUser = { "email": formdata.email, "senha": formdata.senha}
-            const result = await accountService.login(user)
-            authService.saveToken(result.token)
-            const nomeUsuario = await accountService.getUsername(result.id)
-            authService.saveUser(nomeUsuario)
-            toast("Login feito com sucesso!", {
+        if (valido) {
+            const userId = authService.getUserId() // ajuste conforme seu AuthService
+            await livroService.create({ ...formdata, user_id: userId })
+            toast("Livro adicionado com sucesso!", {
                 position: "top-right",
                 autoClose: 5000,
                 pauseOnHover: true,
                 type: "success",
                 theme: "light"
             })
-            if(result.admin){
-                authService.saveAdmin(true)
-                navigate("/admin")
-            } else {
-                navigate("/main")
-            }
-        }
-        else {
+        } else {
             toast("Campos preenchidos incorretamente.", {
                 position: "top-right",
                 autoClose: 5000,
@@ -104,52 +104,53 @@ export default function RegisterForm(){
                 theme: "light"
             })
             Object.keys(messagens).forEach(field => {
-                if(messagens[field as keyof FormErros]){
-                    setFormData(prev => ({
-                        ...prev ,[field]:""
-                    }))
+                if (messagens[field as keyof FormErros]) {
+                    setFormData(prev => ({ ...prev, [field]: "" }))
                 }
             })
         }
     }
 
     const mensagensErros = (erros: FormErros): string[] => {
-        const errosMessagens = Object.values(erros).filter(msg => msg !== "")
-        return errosMessagens
+        return Object.values(erros).filter(msg => msg !== "")
     }
 
-    //FALTA COLOCAR AS VALIDACOES DO EMAIL
-    //FALTA USAR O ZOD
-    //FALTA SE CONECTAR COM A API(MOGOOSE ENV)
-    //FALTA RESETAR O INPUT QUE NÃO FUNCIONAR QUANDO APERTAR EM LOGIN(TALVEZ?)
     const messagens = mensagensErros(erros)
 
-    return(
+    return (
         <div className="MainContainer">
             <div className="LoginForm">
-            <h1 className="LoginText">Login</h1>
-            <form action="" id="formLogin">
-            <h4>Entre na sua conta:</h4>
-            <div className="EmailForm">
-                <label htmlFor="email">Email:</label>
-                <input id="email" type="text" value={formdata.email} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.emailRef}/>
-            </div>
-            <div className="SenhaForm">
-                <label htmlFor="">Senha:</label>
-                <input type="password" id="senha" value={formdata.senha} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.senhaRef}/>
-            </div>
-            </form>
+                <h1 className="LoginText">Adicionar Livro</h1>
+                <form action="" id="formLivro">
+                    <h4>Preencha os dados do livro:</h4>
+                    <div className="EmailForm">
+                        <label htmlFor="nome">Nome:</label>
+                        <input id="nome" type="text" value={formdata.nome} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.nomeRef} />
+                    </div>
+                    <div className="EmailForm">
+                        <label htmlFor="editora">Editora:</label>
+                        <input id="editora" type="text" value={formdata.editora} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.editoraRef} />
+                    </div>
+                    <div className="EmailForm">
+                        <label htmlFor="comentario">Comentário:</label>
+                        <input id="comentario" type="text" value={formdata.comentario} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.comentarioRef} />
+                    </div>
+                    <div className="SenhaForm">
+                        <label htmlFor="quantidade_total">Quantidade:</label>
+                        <input id="quantidade_total" type="number" value={formdata.quantidade_total} onChange={onChangeFormData} onBlur={formBlurs} ref={inputRefs.quantidadeRef} />
+                    </div>
+                </form>
             </div>
             <div className="ButtonsLoginForm">
-                <span><button className="ConfirmLogin" onClick={handleLoginClick}>Login</button></span>
+                <span><button className="ConfirmLogin" onClick={handleSubmitClick}>Adicionar</button></span>
             </div>
             <div className="MensagensErros">
                 {messagens.length > 0 && (
                     <div>
                         <ul>
-                            {messagens.map((error) => (
-                                <li>{error}</li>   
-                            ))} 
+                            {messagens.map((error, index) => (
+                                <li key={index}>{error}</li>
+                            ))}
                         </ul>
                     </div>
                 )}
@@ -157,14 +158,3 @@ export default function RegisterForm(){
         </div>
     )
 }
-
-/**
- * const usernameConstraint: boolean = !!(errors.username.length)
-        const passwordConstraint: boolean = !!(errors.password.length)
-        const passwordConfirmationConstraint: boolean = !!(errors.passwordConfirmation.length)
-        
-        if(passwordConstraint || passwordConfirmationConstraint || usernameConstraint){
-            displayMessages(errors)
-        }
-        return null
- */
