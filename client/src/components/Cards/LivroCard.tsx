990cb299612e85d94658df8fa75c3fbde20678cf
Carlos98770 <eduardoabc.eduardo9@gmail.com>
2026-06-02 23:36:12 -0300

    feat: funcao de deletar livro

--- client/src/components/Cards/LivroCard.tsx
+++ client/src/components/Cards/LivroCard.tsx
@@ -1,31 +1,50 @@
 import React, { useState } from 'react';
 import AdicionarModal from '../../pages/AlterarLivros/AlterarLivro'; // Ajuste o caminho se necessário
 import './LivroCard.css';
+import { LivroService } from '../../services/LivroService';
 
 interface LivroCardProps {
+  id: number
   titulo: string;
   disponivel: boolean;
+  descricao: string;
+  onDelete?: (id: number) => void;
 }
 
-export default function LivroCard({ titulo, disponivel }: LivroCardProps) {
+export default function LivroCard({ id,titulo, disponivel, descricao, onDelete }: LivroCardProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  return (
-    <>
-      <div className="LivroCard">
-        <div className="LivroInfo">
-          <h3 className="LivroTitulo">{titulo}</h3>
-          <span className={`LivroStatus ${disponivel ? 'disponivel' : 'indisponivel'}`}>
-            {disponivel ? 'Disponível' : 'Indisponível'}
-          </span>
-        </div>
-        
-        <button className="BtnAlterarLivro" onClick={() => setIsModalOpen(true)}>
-          Alterar
-        </button>
-      </div>
+  const handleDelete = async () => {
+    if (!confirm(`Deseja excluir "${titulo}"?`)) return;
+    await LivroService.deletar(id);
+    onDelete?.(id);
+  };
 
-      <AdicionarModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}/>
-    </>
-  );
+  return (
+  <>
+    <div className="LivroCard">
+      <div className="LivroInfo">
+        <h3 className="LivroTitulo">{titulo}</h3>
+        <p className="LivroDescricao">{descricao}</p>
+      </div>
+      <div className="CardFooter">
+        <span className={`LivroStatus ${disponivel ? 'disponivel' : 'indisponivel'}`}>
+          <span className="StatusDot" />
+          {disponivel ? 'Disponível' : 'Indisponível'}
+        </span>
+        <div className="CardActions">
+            <button className="BtnAlterarLivro" onClick={() => setIsModalOpen(true)}>
+              Alterar
+            </button>
+            <button className="BtnDeletarLivro" onClick={handleDelete}>
+              Excluir
+            </button>
+          </div>
+      </div>
+    </div>
+    <AdicionarModal isOpen={isModalOpen} 
+                    onClose={() => setIsModalOpen(false)}
+                    livro={{ id, nome: titulo, comentario: descricao }} />
+  </>
+);
 }
