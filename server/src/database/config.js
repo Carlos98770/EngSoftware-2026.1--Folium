ebeda019ea4189cc2ac1a69186eed12a7037b583
JoaoVictorNascimento1 <nascimentojv01@gmail.com>
2026-06-06 15:26:57 -0300

    correcao adicionar banco sqlite em memoria para testes do CI

--- server/src/database/config.js
+++ server/src/database/config.js
@@ -1,23 +1,24 @@
 import { Sequelize } from 'sequelize';
 
 const dbPassword = process.env.DB_PASSWORD || process.env.DB_PASS || '';
+const isTestEnv = process.env.NODE_ENV === 'test';
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
+    isTestEnv ? 'sqlite::memory:' : process.env.DB_NAME,
     process.env.DB_USER,
     dbPassword,
     {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: 'mysql',
-        logging: console.log,
+        dialect: isTestEnv ? 'sqlite' : 'mysql',
+        logging: isTestEnv ? false : console.log,
         timezone: '-03:00',
     }
 );
 
 export async function connectDataBase() {
     await sequelize.authenticate();
-    console.log('MySQL conectado!');
+    console.log(isTestEnv ? 'SQLite (Memória) conectado para testes!' : 'MySQL conectado!');
 }
 
 export default sequelize;
